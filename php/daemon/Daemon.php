7bda8d7ba928f7ddb994ed2ca19e6f73287c12b1
liyunfei <liyunfei@yongche.com>
2016-07-19 12:52:38 +0800

    multi process daemon class

--- php/daemon/Daemon.php
+++ php/daemon/Daemon.php
@@ -11,7 +11,6 @@ spl_autoload_register("Autoload::init");
 class Daemon
 {
     private $_worker;
-    public $amount   = 256;
     public $page     = 1;
     public $pagesize = 1000;
     
@@ -24,38 +23,8 @@ class Daemon
             'worker_num' => 2,
             'daemonize'  => false,
         ));
-        $amount = $this->amount;
-        $this->_worker->onRecive = function(Worker $fd, $message) use ($amount){
-            //处理业务逻辑
-            $fd->log("recive $message");
-            //更新数据库
-            $data = json_decode($message, true);
-            $model = ucwords(str_replace("_", " ", $data['table']));
-            $model = str_replace(" ", "", $model);
-            unset($data['table']);
-            //echo $model;
-            if($model == "OrderTransactionHistory") $model = 'OrderSettlementHistory';
-            require_once '../../application/bootstrap.php';
-            $model = "\\Settlement\\Model\\".$model;
-            $config = array('model'=>$model, 'number'=>$data['driver_id'] % 256);
-            $db_model = Register::get($config);
-            if(!$db_model){
-                $db_model = new $model($data['driver_id']);
-                Register::set($config, $db_model);
-            }
-            $child = array();
-            if(isset($data['child'])){
-                $child = $data['child'];
-                unset($data['child']);
-            }
-            $id = $db_model->insertRecord($data);
-            //$id = 1;
-            if($child){
-                $child[$db_model->_primaryKey] = $id;
-                //递归调用
-                call_user_func($fd->onRecive, $fd, json_encode($child));
-            }
-            $fd->log("result $id");
+        $this->_worker->onRecive = function(Worker $fd, $message){
+            //子进程业务逻辑
         };
         
         $this->_worker->run();
@@ -69,33 +38,7 @@ class Daemon
      */
     public function work($config, $table, $arr_fields, $arr_where=array(), $func=null){
         try{
-            $db = new Database($config);
-            
-            $primary = $arr_fields[0];
-            $id = 0;
-            $flag = true;
-            $data = array();
-            while($flag){
-                $arr_where[$primary] = array('>'=>$id);
-                $orderby = "order by $primary asc";
-                $data = $db->queryList($table, implode(",", $arr_fields), $arr_where, $this->page, $this->pagesize, $orderby);
-                //print_r($data);
-                if($func){
-                    $data = $func($data, $db);
-                }
-                //print_r($data);exit();
-                if(count($data) < $this->pagesize){
-                    $flag = false;
-                }else{
-                    $id = $data[count($data)-1][$primary];
-                }
-                foreach ($data as $key => $value){
-                    unset($value[$primary]);//删除主键id
-                    $value['table'] = $table;
-                    $this->_worker->send(json_encode($value), $value['driver_id'] % $this->amount);//发送到子进程
-                }
-                usleep(100);
-            }
+            //任务分发逻辑
         } catch (Exception $e){
             
         }
---
+++ php/daemon/test.php
@@ -0,0 +1,25 @@
+<?php 
+
+include 'Worker.php';
+
+$worker = new Worker();
+
+$worker->onRecive = function(Worker $fd, $message){
+    //处理业务逻辑
+    $fd->log("recive $message");
+    //更新数据库
+};
+
+$worker->run();
+
+$start_time = time();
+
+for($i =0; $i < 100000; $i++){
+    $worker->send(time().rand(10000, 99999));
+}
+
+$worker->stop();
+
+$end_time = time();
+
+echo "100000 use ".($start_time - $end_time)."s";
